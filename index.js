87d8b80a33bc4f1f3a68f4f67367df6764d952eb
moneychaudhary <money.chaudhary11@gmail.com>
2019-03-26 19:32:01 +0530

    sample skill completed

--- index.js
+++ index.js
@@ -11,50 +11,76 @@ app.post('/', (req, res) => {
 });
 
 async function handleRequest(event, response) {
+  let res;
+  let request = event.request;
   try {
-    let res;
-    let request = event.request;
     if(request.type === "LaunchRequest") {
-      let options = {
-        speechText: "Welcome to Self hosted skill. Say play short audio to listen to a audio clip",
-        repromptText: "You can say play short audio",
-        shouldEndSession: false
-      };
-      res = buildResponse(options);
-      await pulse.log(request, res);
-      response.send(res);
+      res = handleLaunchRequest();
     } else if(request.type === "IntentRequest") {
-      let options = {};
-      if(request.intent.name === "ShortAudioIntent") {
-        options.speechText ="<audio src='https://s3-us-west-2.amazonaws.com/jessie.stieger/pulselabsprofessor/Intro.1.mp3' />";
-        options.speechText+= "Now say play long form audio to play using audio player";
-        options.shouldEndSession = false;
-      } else {
-        options.speechText = "Sorry I don't know how to respond to that";
-      }
-      res = buildResponse(options);
-      // pulse.log(request, res);
-      response.send(res);
+      res = handleIntentRequest(request);
     } else if(request.type === "SessionEndedRequest") {
-      let options = {
-        speechText: "Bye for now",
-        shouldEndSession: true
-      };
-      response.send(buildResponse(options));
+      res = handleSessionEndedRequest();
     } else {
-      let options = {
-        speechText: "Got unknown request type",
-        shouldEndSession: true
-      };
-      response.send(buildResponse(options));
+      res = handleUnknownRequest();
     }
   } catch (e) {
-    let options = {
-      speechText: "Some Error occurred",
-      shouldEndSession: true
-    };
-    response.send(buildResponse(options));
+    res = handleError();
+  } finally {
+    await pulse.log(request, res);
+    response.send(res);
   }
+}
+
+function handleLaunchRequest() {
+  let options = {
+    speechText: "Welcome to demo self hosted skill. Say play audio to listen to a audio clip",
+    repromptText: "You can say play short audio or help",
+    shouldEndSession: false
+  };
+  return buildResponse(options);
+}
+
+function handleIntentRequest(request) {
+  let options = {};
+  if(request.intent.name === "ShortAudioIntent") {
+    //TODO:: Change the audio file used here
+    options.speechText ="<audio src='https://s3-us-west-2.amazonaws.com/jessie.stieger/pulselabsprofessor/Intro.1.mp3' />";
+    options.shouldEndSession = false;
+  } else if(request.intent.name === "AMAZON.StopIntent" || request.intent.name === "AMAZON.CancelIntent") {
+    options.speechText = "Exiting from the skill";
+    options.shouldEndSession = true;
+  } else if(request.intent.name === "AMAZON.HelpIntent") {
+    options.speechText = "You can say play audio to listen to audio clip or stop to exit from skill";
+    options.shouldEndSession = false;
+  } else {
+    options.speechText = "Sorry I don't know how to respond to that. Exiting you from the skill";
+    options.shouldEndSession = true;
+  }
+  return buildResponse(options);
+}
+
+function handleSessionEndedRequest() {
+  let options = {
+    speechText: "Bye for now",
+    shouldEndSession: true
+  };
+  return buildResponse(options);
+}
+
+function handleUnknownRequest() {
+  let options = {
+    speechText: "Got unknown request type. Exiting you from the skill",
+    shouldEndSession: true
+  };
+  return buildResponse(options);
+}
+
+function handleError() {
+  let options = {
+    speechText: "Some error occurred while processing the requested information",
+    shouldEndSession: true
+  };
+  return buildResponse(options);
 }
 
 function buildResponse(options) {
@@ -77,6 +103,29 @@ function buildResponse(options) {
         ssml: "<speak>" + options.repromptText + "</speak>"
       }
     }
+  }
+
+  if(options.cardTitle){
+    response.response.card = {
+      type: "Simple",
+      title: options.cardTitle
+    };
+
+    if(options.imageUrl){
+      response.response.card.type = "Standard";
+      response.response.card.text = options.cardContent;
+      response.response.card.image = {
+        smallImageUrl: options.imageUrl,
+        largeImageUrl: options.imageUrl
+      };
+    }
+    else{
+      response.response.card.content = options.cardContent;
+    }
+  }
+
+  if("session" in options && "attributes" in options.session) {
+    response.sessionAttributes = options.session.attributes;
   }
 
   return response;
